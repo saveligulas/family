e301950b669081ab96a6362b18c1498fdc74f675
saveligulas <saveli.gulas@gmail.com>
2023-11-20 14:09:40 +0100

    updates

--- src/main/java/gulas/family/family/home/UserRepository.java
+++ src/main/java/gulas/family/family/home/UserRepository.java
@@ -1,8 +1,20 @@
 package gulas.family.family.home;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.username = ?1")
+    Optional<User> findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.cryptKey.privateKey = ?1")
+    Optional<User> findByPrivateKey(byte[] value);
+
+    @Query("SELECT u FROM User u WHERE u.cryptKey.publicKey = ?1")
+    Optional<User> findByPublicKey(byte[] value);
 }

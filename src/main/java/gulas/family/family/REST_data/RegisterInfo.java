4994cf22990f9d13bed61d93eb3d487ee69adc47
saveligulas <saveli.gulas@gmail.com>
2023-11-20 14:10:09 +0100

    updates

--- src/main/java/gulas/family/family/REST_data/RegisterInfo.java
+++ src/main/java/gulas/family/family/REST_data/RegisterInfo.java
@@ -1,9 +1,12 @@
 package gulas.family.family.REST_data;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 public class RegisterInfo {
     private String firstName;
     private String lastName;
